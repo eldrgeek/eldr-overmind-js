707e7b87c963e03597e6c2d8e65369b4bc81e6e9
Mike Wolf <mw@mike-wolf.com>
2019-10-17 01:00:37 -0400

    clean up code in current module

--- src/CurrentModule.jsx
+++ src/CurrentModule.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { app } from './app';
 import { Provider } from 'overmind-react';
+import { app } from './app';
 import { useApp } from './app';
+
 const CurrentModule = Element => {
   const rootElement = document.getElementById('root');
   render(
